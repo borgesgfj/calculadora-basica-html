f3aab4e3edc507d6fd67a30a6799439e227c19c0
Gilberto Borges <borgesgfj@gmail.com>
2021-07-06 09:20:34 -0300

    versão final do Script

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-let n1 = parseInt(document.getElementById("cNum1").value)
-let n2 = parseInt(document.getElementById("cNum2").value)
-
-function resultado(numero1, numero2) {
-let selecOper = document.getElementById("cOperacoes").value
-let calc = 0
-if (selecOper == "soma") {
-calc = numero1 + numero2
-} else if (selecOper == "sub") {
-calc = numero1 - numero2
-} else if (selecOper == "division") {
-calc = numero1 / numero2
-} else
-calc = numero1 * numero2
-return calc
+function resultado() {
+  let n1 = parseInt(document.getElementById("cNum1").value);
+  let n2 = parseInt(document.getElementById("cNum2").value);
+  let selecOper = document.getElementById("cOperacoes").value;
+  let calc = 0;
+  console.log(n1, n2, selecOper);
+  if (selecOper == "soma") {
+    calc = n1 + n2;
+  } else if (selecOper == "sub") {
+    calc = n1 - n2;
+  } else if (selecOper == "division") {
+    calc = n1 / n2;
+  } else {
+    calc = n1 * n2;
+  }
+  console.log(calc)
+  document.getElementById("cResult").value = calc;
 }
-
-document.getElementById("cResult").value = resultado(n1, n2)
